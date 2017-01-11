37ba317536545aff7426c609a0b3794ca6d343f8
ShineHua2017 <20497409@qq.com>
2017-01-11 14:20:48 +0800

    Create CM32181_arduino.h

---
+++ CM32181_Arduino/CM32181_arduino.h
@@ -0,0 +1,32 @@
+/*
+  CM32181_arduino.h - Library for flashing Morse code.
+  Created by Shine Hua 2017.01.11 
+  Released into the public domain.
+*/
+
+#ifndef CM32181_arduino_h
+#define CM32181_arduino_h
+
+#include <Arduino.h>
+ 
+#define CM32181_REG_ADDR_CMD            0x00
+#define CM32181_REG_ADDR_ALS            0x04
+#define CM32181_REG_ADDR_STATUS         0x06
+#define CM32181_REG_ADDR_ID             0x07
+
+class CM32181
+{
+  public:
+    CM32181(uint8_t address);
+    boolean init_chip(void);
+    uint16_t read_sensor_raw(void);
+	uint16_t get_chip_ID(void);
+	uint16_t get_chip_status(void);
+	boolean powerdown_chip(void);
+	boolean powerup_chip(void);
+  private:
+    uint8_t _address;
+	void sensorWait(int time);
+};
+ 
+#endif
